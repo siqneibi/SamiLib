2a275e7649ef0d601b38b3d3481a4e45048c19fd
Sami Iqneibi <sami_iqneibi@sedco-online.com>
2024-02-08 10:04:36 +0300

    Initial project setup

---
+++ SamiLib/SamiLib.h
@@ -0,0 +1,18 @@
+//
+//  SamiLib.h
+//  SamiLib
+//
+//  Created by Sami Iqneibi on 07/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SamiLib.
+FOUNDATION_EXPORT double SamiLibVersionNumber;
+
+//! Project version string for SamiLib.
+FOUNDATION_EXPORT const unsigned char SamiLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SamiLib/PublicHeader.h>
+
+
---
+++ SamiLibExamples/SamiLibExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamiLibExamplesApp.swift
+//  SamiLibExamples
+//
+//  Created by Sami Iqneibi on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SamiLibExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
